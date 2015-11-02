96e31fc2119079bb96ec7a1205d708e43f650f9f
Kater-auf-Dach <kateraufdach@yandex.ru>
2015-11-02 20:07:07 +0300

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function toggleMobileMenu() {
+  var openMenu = document.querySelector('.touch-menu'),
+      menuMain = document.querySelector('.main-menu');
+  if(openMenu.classList) {
+    openMenu.addEventListener('click', function(event){
+      event.preventDefault();
+      menuMain.classList.toggle("menu-show")}
+    )}
+  else {
+    window.onload = function (){
+      document.getElementById('touch-menu').onclick = openMenuIE;
+    };
+    function openMenuIE (){
+      var menuIE = document.getElementById('nav');
+      if(menuIE.style.display!='block'){
+    		menuIE.style.display='block';
+    	}
+    	else
+    	{
+    		menuIE.style.display= 'none';
+    	}
+    }
+  }
+}
+
+toggleMobileMenu();
